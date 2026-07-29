d8e01c97a814987f348fc40fdc68158a70f2b617
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-07-30 04:27:23 +0800

    Update factorial_manual.py
    add a final sentence

--- factorial_manual.py
+++ factorial_manual.py
@@ -1,11 +1,10 @@
-import math
-
 print("Welcome to factorial calculator")
 number = int(input("Input number : "))
+counter = number
 i = number-1
 while i>0:
     number*=i
     i-=1
-print(number)
+print("The factorial number of",counter,"is",number)
 
 
